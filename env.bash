947206961cbd9cee97bbef438d9249f76afea43b
Wig C <onlywig@gmail.com>
2017-01-23 15:40:37 +0800

    nutsboard: add nutsboard almond+walnut support.

--- env.bash
+++ env.bash
@@ -1,6 +1,6 @@
 TOP="${PWD}"
-PATH_KERNEL="${PWD}/tn-kernel"
-PATH_UBOOT="${PWD}/uboot-imx"
+PATH_KERNEL="${PWD}/linux-am335x"
+PATH_UBOOT="${PWD}/u-boot"
 LINUX_ROOTFS=lede-omap-default-rootfs.tar.gz
 
 export PATH="${PATH_UBOOT}/tools:${PATH}"
@@ -30,9 +30,21 @@ elif [[ "$CPU_TYPE" == "am335x" ]]; then
         if [[ "$BASEBOARD" == "st7b2" ]]; then
             UBOOT_CONFIG='am335x_st7b2_defconfig'
             KERNEL_IMAGE='zImage'
-            KERNEL_CONFIG='tailyn_am335x-evm_defconfig'
+            KERNEL_CONFIG='nutsboard_defconfig'
             DTB_TARGET='am335x-st7b2.dtb'
         fi
+    fi
+
+elif [[ "$CPU_TYPE" == "nutsboard" ]]; then
+    if [[ "$CPU_MODULE" == "almond" ]]; then
+        if [[ "$BASEBOARD" == "walnut" ]]; then
+            UBOOT_CONFIG='nutsboard_almond_defconfig'
+            KERNEL_IMAGE='zImage'
+            KERNEL_CONFIG='nutsboard_defconfig'
+            DTB_TARGET='am335x-nutsboard-almond.dtb'
+        fi
+
+
     fi
 fi
 
@@ -173,4 +185,22 @@ flashcard() {
       sudo umount mnt
       rm -rf mnt
    fi
+}
+
+ubi_create() {
+
+  local TMP_PWD="${PWD}"
+  sd_node="$@"
+  echo $sd_node
+  mkdir mnt
+  sudo  mount ${sd_node}2 mnt
+
+  sudo mkfs.ubifs -F -q -r mnt -m 2048 -e 126976 -c 2047 -o ubifs.img
+  sync;
+  sudo ubinize -o ubi.img -m 2048 -p 128KiB -s 2048 cookers/ubinize.cfg
+  sync;
+
+  sudo rm ubifs.img
+  sudo umount mnt
+  rm -rf mnt
 }
